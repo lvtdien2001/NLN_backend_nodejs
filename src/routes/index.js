80151ae4d64ca22a14da8f399d8cae575e3b0a41
Le-Van-Thanh-Dien-B1906651 <dienb1906651@student.ctu.edu.vn>
2023-03-04 19:56:24 +0700

    Test Connect MongoDB Aslat

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 
 
 const authRouter = require('./Auth.route');
-const productRouter = require('./Product.route');
+//const productRouter = require('./Product.route');
 function route(app) {
    
   
     app.use('/api/auth', authRouter);
-    app.use('/api/products', productRouter);
+    //app.use('/api/products', productRouter);
     
     
 }
