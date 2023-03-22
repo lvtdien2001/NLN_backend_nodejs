d4045bfb68d32a387d08c6419f819ecdd1c3be80
Ky <thinh2006uu@gmail.com>
2023-03-22 14:02:32 +0700

    Update product and address

---
+++ src/models/Address.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AddressSchema = Schema({
+    phone: {
+        type: String
+    },
+    fullName: {
+        type: String
+    },
+    province: {
+        type:String
+    },
+    provinceCode: {
+        type: Number
+    },
+    district: {
+        type:String
+    },
+    districtCode: {
+        type: Number
+    },
+    ward: {
+        type:String
+    },
+    wardCode: {
+        type: Number
+    },
+    description: {
+        type: String
+    }
+    
+}, {timestamps: true});
+
+
+module.exports = mongoose.model('addresses', AddressSchema);
